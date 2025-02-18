73981df059750cea1afd57bb89a208201dd14380
raganubeka <raganu@beka.com>
2025-02-18 08:50:02 +0200

    18.01

---
+++ datoriummerch/Controllers/MerchController.cs
@@ -0,0 +1,23 @@
+﻿using datoriummerch.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace datoriummerch.Controllers
+{
+    [Route("api/datorium-merch")]
+    [ApiController]
+    public class MerchController
+    {
+        private readonly MerchContext _dbContext;
+
+        public MerchController(MerchContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet("ok")]
+        public ActionResult<string> GetMerch()
+        {
+            return "OK";
+        }
+    }
+}
